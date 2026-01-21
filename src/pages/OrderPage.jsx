439b89234278cbdd6cb0ecdaaa5b6b9419561a8b
trendswtv-creator <trendswtv@gmail.com>
2026-01-22 00:25:40 +0100

    Update OrderPage.jsx

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -23,7 +23,7 @@ const OrderPage = () => {
     const formData = new FormData(form);
 
     try {
-      const response = await fetch("https://formspree.io/f/FORM_ID", {
+      const response = await fetch("https://formspree.io/f/mykkaylk", {
         method: "POST",
         body: formData,
         headers: {
